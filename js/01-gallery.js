798add168459a88fdff96bb1cac1df3480ce4559
BanderaGaming <Glory.To.Ukraine.1648@gmail.com>
2023-05-04 13:04:41 +0200

    fix1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,20 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const gallery = document.querySelector(".gallery");
+const galleryMaker = () => {
+    galleryItems.forEach(img =>
+        gallery.insertAdjacentHTML("afterbegin",
+            `<li class="gallery__item"><a class="gallery__link"><img class="gallery__image" data-source="${img.original}" scr="${img.preview}" alt="${img.description}"></a></li>`)
+    );
+};
 
+gallery.addEventListener("click", (event) => {
+    if (event.target.nodeName !== "A") {
+        return;
+    }
+
+}   
+);
+console.log(_.add(3,4) )
+galleryMaker()
 console.log(galleryItems);
